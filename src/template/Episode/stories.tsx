24e80535113fb3c6f90c8d41bacc53fe7336f792
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-12-24 15:28:33 -0300

    feat: remove storybook in templates

--- src/template/Episode/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Episode from '.'
-
-export default {
-  title: 'Episode',
-  component: Episode
-} as Meta
-
-export const Basic: Story = (args) => <Episode {...args} />
--- src/template/Episodes/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Episodes from '.'
-
-export default {
-  title: 'Episodes',
-  component: Episodes
-} as Meta
-
-export const Basic: Story = (args) => <Episodes {...args} />
